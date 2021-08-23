0ba4864f695f4fea9ea95603bf7916073506af3b
JUOSORIOT <jaot12345@gmail.com>
2021-08-23 13:49:47 -0500

    Version Final

--- Entrega_1/Entrega_1.ino
+++ Entrega_1/Entrega_1.ino
@@ -2,12 +2,12 @@ bool sonIguales(char *claveCorrecta, char *claveIngresada)
 {
   int auxiliar = 0;
   for (int i = 0; i < 6; i++) {
-    if(claveCorrecta[i]==claveIngresada[i])
+    if (claveCorrecta[i] == claveIngresada[i])
     {
       auxiliar = auxiliar + 1;
     }
   }
-  if(auxiliar == 6)
+  if (auxiliar == 6)
   {
     return true;
   }
@@ -19,13 +19,14 @@ bool sonIguales(char *claveCorrecta, char *claveIngresada)
 void setup() {
   Serial.begin(115200);
 }
-void tarea2(){
+
+void tarea2() {
   static uint32_t previousMillis = 0;
   static const uint32_t interval = 500;
   static bool taskInit = false;
   static const uint8_t ledPin =  3;
   static uint8_t ledState = LOW;
-  if(taskInit == false){
+  if (taskInit == false) {
     pinMode(ledPin, OUTPUT);
     taskInit = true;
   }
@@ -43,65 +44,75 @@ void tarea2(){
 void tarea1() {
   static int estado = 1;
   static int conteo = 20;
-  static int previousMillis = 0;
-  int currentMillis = millis();
-  static int intervalo = 1000;
+  static unsigned long previousMillis = 0;
+
+  static unsigned long intervalo = 1000;
   static int8_t rxData[10];
-  static char codigoCorrecto[6] = {'d','u','d','d','u','a'};
+  static char codigoCorrecto[6] = {'d', 'u', 'd', 'd', 'u', 'a'};
   static char codigoIngresado[6];
   static int contador = 0;
+
+  unsigned long currentMillis = millis();
+
+
   switch (estado) {
     case 1: {
-      static char dato;
-      if(Serial.available() > 0)
-      {
-        dato = Serial.read();
-        if((char)dato == 'd' && conteo >= 11 && conteo <= 60)
+        if (Serial.available() > 0)
         {
-          conteo = conteo - 1;       
-        }
-        else if((char)dato == 'u' && conteo >= 10 && conteo <= 59)
-        {
-          conteo = conteo + 1;
-        }
-        else if((char)dato == 'a')
-        {
-          estado = 2;        
-        }
-        dato = ' ';
-      }
-      Serial.println(conteo);
-      break;
-    }
-    case 2: {
-      if(Serial.available() > 0){
-        codigoIngresado[contador] = Serial.read();
-        contador++;
-        if(contador == 6){
-          if(sonIguales(codigoCorrecto, codigoIngresado))
+          char dato = Serial.read();
+          if ((char)dato == 'd' && conteo >= 11)
           {
+            conteo = conteo - 1;
+            Serial.println(conteo);
+          }
+          else if ((char)dato == 'u' && conteo <= 59)
+          {
+            conteo = conteo + 1;
+            Serial.println(conteo);
+          }
+          else if ((char)dato == 'a')
+          {
+            estado = 2;
+            previousMillis = millis();
+            contador = 0;
+
+          }
+        }
+
+        break;
+      }
+    case 2: {
+        if (Serial.available() > 0) {
+          codigoIngresado[contador] = Serial.read();
+          contador++;
+          if (contador == 6) {
+            if (sonIguales(codigoCorrecto, codigoIngresado))
+            {
+              estado = 1;
+              conteo = 20;
+            }
+            contador = 0;
+          }
+        }
+        if ( (millis() - previousMillis) >= intervalo)
+        {
+          previousMillis = millis();
+          if (conteo > 0)
+          {
+            conteo = conteo - 1;
+            Serial.println(conteo);
+          }
+          else
+          {
+            Serial.println("Boom");
             estado = 1;
             conteo = 20;
+
           }
-          contador = 0;
         }
+
+        break;
       }
-      if ( (currentMillis - previousMillis) >= intervalo)
-      {
-        previousMillis = currentMillis;
-        if(conteo> 0)
-        {
-          conteo = conteo - 1;
-        }
-        else
-        {
-          estado = 1;
-          conteo = 20;
-        }
-      }
-      Serial.println(conteo);
-      break;
-    }
     default:
       Serial.println("Error");
       break;
